65579f5309901e2b50e3b0e112af6dd91c96a081
oihan <oihan.esteban.oe@gmail.com>
2019-01-29 11:45:47 +0100

    test du mardi

---
+++ src/Git/MyProgram.java
@@ -0,0 +1,9 @@
+package Git;
+
+public class MyProgram {
+	
+	public static void main(String[] args) {
+		
+	} // end of the main
+
+} // end of the class
---
+++ src/MyProgram.java
@@ -0,0 +1,10 @@
+package default;
+
+public class MyProgram {
+
+	
+	public static void main(String[] args) {
+		
+	} // end of the main
+	
+} // end of the class
